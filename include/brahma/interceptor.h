a1c69bf2646fb6a59ad27659d32a0e347cda9f54
Hariharan Devarajan <mani.hariharan@gmail.com>
2022-08-16 10:34:58 -0700

    Added new macro for calling gotcha wrappee.

--- include/brahma/interceptor.h
+++ include/brahma/interceptor.h
@@ -6,32 +6,35 @@
 #define BRAHMA_INTERCEPTOR_H
 
 #include <gotcha/gotcha.h>
+
 #include <memory>
 
 const int MAX_API_COUNT = 1024;
 static gotcha_binding_t bindings[MAX_API_COUNT];
 static int current_api_index = 0;
-#define GOTCHA_BINDING_MACRO(fname) \
-    bindings[current_api_index].name = #fname; \
-    bindings[current_api_index].wrapper_pointer = (void*) fname##_wrapper; \
-    bindings[current_api_index].function_handle = & fname##_handle;        \
-    current_api_index++;
+#define GOTCHA_BINDING_MACRO(fname)                                     \
+  bindings[current_api_index].name = #fname;                            \
+  bindings[current_api_index].wrapper_pointer = (void*)fname##_wrapper; \
+  bindings[current_api_index].function_handle = &fname##_handle;        \
+  current_api_index++;
 
 #define GOTCHA_MACRO(name, ret, args, args_val, class_name) \
-typedef ret (*name##_fptr) args; \
-static gotcha_wrappee_handle_t name##_handle; \
-ret name##_wrapper args {             \
-    return class_name::get_instance()->name args_val;    \
-};
+  typedef ret(*name##_fptr) args;                           \
+  static gotcha_wrappee_handle_t name##_handle;             \
+  ret name##_wrapper args { return class_name::get_instance()->name args_val; };
 
 #define BRAHMA_WRAPPER(name) name##_wrapper;
 
-#define BRAHMA_UNWRAPPED_FUNC(name, ret, args) \
-fprintf(stderr, "[BRAHMA]\tFunction %s() not wrapped. Calling Original.\n", #name); \
-name##_fptr name##_wrappee = (name##_fptr) gotcha_get_wrappee(name##_handle); \
-ret result = name##_wrappee args; \
-return result;
+#define BRAHMA_UNWRAPPED_FUNC(name, ret, args)                                 \
+  fprintf(stderr, "[BRAHMA]\tFunction %s() not wrapped. Calling Original.\n",  \
+          #name);                                                              \
+  name##_fptr name##_wrappee = (name##_fptr)gotcha_get_wrappee(name##_handle); \
+  ret result = name##_wrappee args;                                            \
+  return result;
 
+#define BRAHMA_MAP_OR_FAIL(func_)                                         \
+  auto __real_##func_ = (func_##_fptr)gotcha_get_wrappee(func_##_handle); \
+  assert(__real_##func_ != NULL)
 int update_posix();
 
 int update_stdio();
@@ -39,11 +42,11 @@ int update_stdio();
 int update_mpiio();
 
 int bind_functions() {
-    current_api_index = 0;
-    update_posix();
-    update_stdio();
-    update_mpiio();
-    return 0;
+  current_api_index = 0;
+  update_posix();
+  update_stdio();
+  update_mpiio();
+  return 0;
 }
 
-#endif //BRAHMA_INTERCEPTOR_H
+#endif  // BRAHMA_INTERCEPTOR_H
