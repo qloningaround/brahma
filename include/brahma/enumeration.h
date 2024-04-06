ab9f48fbe0a4b45588423dd1efbb77797d259b05
Hariharan Devarajan <mani.hariharan@gmail.com>
2024-04-06 18:49:45 +0000

    missing import for gcc 13
    we need an explicit import for uint8_t

--- include/brahma/enumeration.h
+++ include/brahma/enumeration.h
@@ -4,7 +4,8 @@
 
 #ifndef BRAHMA_ENUMERATION_H
 #define BRAHMA_ENUMERATION_H
+#include <cstdint>
 namespace brahma {
-enum InterfaceType:uint8_t { INTERFACE_POSIX = 0, INTERFACE_STDIO = 1, INTERFACE_MPIIO = 2, INTERFACE_HDF5 = 3 };
+enum InterfaceType : uint8_t { INTERFACE_POSIX = 0, INTERFACE_STDIO = 1, INTERFACE_MPIIO = 2, INTERFACE_HDF5 = 3 };
 }
 #endif  // BRAHMA_ENUMERATION_H
