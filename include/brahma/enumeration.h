70902543b4142afe00d7bb6bfa1ba3461a70caf9
qloningaround <111423755+qloningaround@users.noreply.github.com>
2024-04-16 16:44:50 +0300

    Update enumeration.h

--- include/brahma/enumeration.h
+++ include/brahma/enumeration.h
@@ -6,6 +6,6 @@
 #define BRAHMA_ENUMERATION_H
 #include <cstdint>
 namespace brahma {
-enum InterfaceType : uint8_t { INTERFACE_POSIX = 0, INTERFACE_STDIO = 1, INTERFACE_MPIIO = 2, INTERFACE_HDF5 = 3 };
+enum InterfaceType { INTERFACE_POSIX = 0, INTERFACE_STDIO = 1, INTERFACE_MPIIO = 2, INTERFACE_HDF5 = 3 };
 }
 #endif  // BRAHMA_ENUMERATION_H
